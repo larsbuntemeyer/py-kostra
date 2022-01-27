d5b07ff69518a7581e8623da721f88ff8719eb81
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2022-01-27 23:58:39 +0100

    added notebook and cleanup

--- pykostra.py
+++ pykostra.py
@@ -1,16 +1,25 @@
+
+import os
+import glob
+from pathlib import Path
+
+import pandas as pd
+import xarray as xr
+    
+
 duration_name = 'duration'
 interval_name = 'interval'
+
+geoinfo = pd.read_excel("./raw/KOSTRA-DWD-2010R_geog_Bezug.xlsx", sheet_name="Raster_geog_Bezug", index_col='index_rc')
 
 
 def parse_duration_level(f):
     """parse duration level from filename f"""
-    from pathlib import Path
     stem = Path(f).stem
     return stem.split('_')[2]
 
 def kog(f):
     """parse KOG from filename f"""
-    from pathlib import Path
     stem = Path(f).stem
     try:
         return stem.split('_')[3] == "KOG"
@@ -71,3 +80,27 @@ def combine(dsets):
     HN = xr.concat([ds for ds in dsets if "HN" in ds], dim=duration_name)
     #return HN.sortby(HN.duration_level), HN_KOG.sortby(HN_KOG.duration_level)
     return xr.merge([HN, HN_KOG]).sortby(HN[duration_name])
+
+
+def unzip(f, targetdir):
+    """unzip file f to targetdir"""
+    import zipfile
+    with zipfile.ZipFile(f,"r") as zip_ref:
+        zip_ref.extractall(targetdir)
+        
+
+def get_csv_files(raw_dir="./raw", unzip_dir="./unzip"):
+    url = os.path.join(raw_dir,"*.zip")
+    zip_files = glob.glob(url)
+    for f in zip_files:
+        unzip(f, unzip_dir)
+    return glob.glob(os.path.join(unzip_dir, "*.csv"))
+
+
+def kostra_to_dataset(csv_files=None, path="raw"):
+    if csv_files is None:
+        csv_files = get_csv_files(path)
+    tables = [create_table(f) for f in csv_files]
+    dsets = [to_xarray(df) for df in tables]
+    dsets = [add_coords(ds) for ds in dsets]
+    return combine(dsets)
